64e9e8fc7187ff4d267867c30fe463db37719b7a
Joseph Weir <2365352w@student.gla.ac.uk>
2021-10-13 22:17:27 +0100

    Now have a drag-and-drop hexagon setup

--- Logic Gates Educational Game/Assets/GridSystem/Scripts/Circuit.cs
+++ Logic Gates Educational Game/Assets/GridSystem/Scripts/Circuit.cs
@@ -4,76 +4,41 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Circuit : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class Circuit : MonoBehaviour
 {
-    private Vector3 position;
-    private int x_customGridPosition, y_customGridPosition;
-    public CustomGrid myGrid; // This shouldnt be here
+    private float x_startPos, y_startPos;
+    private bool mouseDown = false;
 
-    // next two are from tutorial
-    private Canvas canvas;
-    private GraphicRaycaster graphicRaycaster;
-
-    public Circuit(Vector3 initialPosition)
+    void Update()
     {
-        this.position = initialPosition;
-        renderCircuit(null, initialPosition);
-    }
-    public Circuit Init(Vector3 initialPosition)
-    {
-        return new Circuit(initialPosition);
-    }
-
-    private void renderCircuit(Transform parent, Vector3 localPosition)
-    {
-        GameObject gameObject = new GameObject("Circuit", typeof(SpriteRenderer));
-        Transform transform = gameObject.transform;
-        transform.SetParent(parent, false);
-        transform.localPosition = localPosition;
-
-        SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-        spriteRenderer.drawMode = SpriteDrawMode.Sliced;
-        Sprite circuit = Resources.Load<Sprite>("Sprites/Circuit");
-        spriteRenderer.sprite = circuit;
-        spriteRenderer.size = new Vector2(5f, 5f);
-        spriteRenderer.color = Color.blue;
-    }
-
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        Debug.Log("Began dragging");
-        // Start moving object from the beginning!
-        transform.localPosition += new Vector3(eventData.delta.x, eventData.delta.y, 0) / transform.lossyScale.x;
-
-        myGrid.clearSquare(this.x_customGridPosition, this.y_customGridPosition);
-        // Remove from position inside CustomGrid; 
-        // update position;
-
-        if (!canvas)
+        if (mouseDown)
         {
-            canvas = GetComponentInParent<Canvas>();
-            graphicRaycaster = canvas.GetComponent<GraphicRaycaster>();
+            Vector3 mousePosition;
+            mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            this.gameObject.transform.localPosition = new Vector3(mousePosition.x - x_startPos, mousePosition.y - y_startPos, 0);
         }
-        // Change parent of our item to the canvas.
-        transform.SetParent(canvas.transform, true);
-        // And set it as last child to be rendered on top of UI.
-        transform.SetAsLastSibling();
     }
 
-    public void OnDrag(PointerEventData eventData)
+    private void OnMouseDown()
     {
-        Debug.Log("Mid drag");
-        transform.localPosition += new Vector3(eventData.delta.x, eventData.delta.y, 0) / transform.lossyScale.x;
-        //update position;
+        Debug.Log("Circuit OnMouseDown");
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition;
+            mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            x_startPos = mousePosition.x - this.transform.localPosition.x;
+            y_startPos = mousePosition.y - this.transform.localPosition.y;
+            mouseDown = true;
+        }
     }
 
-    public void OnEndDrag(PointerEventData eventData)
+    private void OnMouseUp()
     {
-        Debug.Log("Dropped");
-        // Get current position;
-        Vector3 currentPosition = new Vector3(3, 4);
-
-        // Place as element inside CustomGrid array;
-        this.position = myGrid.placeElementInGrid(currentPosition);
+        Debug.Log("Circuit OnMouseUp");
+        mouseDown = false;
     }
 }
--- Logic Gates Educational Game/Assets/GridSystem/Scripts/CustomGridTest.cs
+++ Logic Gates Educational Game/Assets/GridSystem/Scripts/CustomGridTest.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class CustomGridTest : MonoBehaviour
 {
+    //public GameObject circuitObject;
+
+    // public GameObject customGridObject;
+    //private Circuit circuitScript;
+    // private CustomGrid customGridScript;
+
+    void Awake()
+    {
+        //circuitScript = circuitObject.GetComponent<Circuit>();
+        Debug.Log("AWAKE");
+        // customGridScript = customGridObject.GetComponent<CustomGrid>();
+    }
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("START");
         int width = 8;
         int height = 6;
         float cellSize = 10f;
         CustomGrid myGrid = new CustomGrid(width, height, cellSize);
 
-        Vector3 initialCircuitPosition = new Vector3(5f, 0.7f);
+        //circuitScript.renderCircuit(null, new Vector3(0, 0));
+
+        // Vector3 initialCircuitPosition = new Vector3(5f, 0.7f);
         // Circuit myCircuit = new Circuit(initialCircuitPosition);
-        Circuit myCircuit = gameObject.AddComponent<Circuit>().Init(initialCircuitPosition);
+        // Circuit myCircuit = gameObject.AddComponent<Circuit>().Init(initialCircuitPosition);
         // myCircuit.instantiate();
     }
 }
