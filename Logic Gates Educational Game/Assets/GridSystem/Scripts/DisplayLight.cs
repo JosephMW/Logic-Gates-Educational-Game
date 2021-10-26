0b03b4748e0661025661c90e2611ac3441ecf140
Joseph Weir <2365352w@student.gla.ac.uk>
2021-10-26 22:04:47 +0100

    changed colours of lights

--- Logic Gates Educational Game/Assets/GridSystem/Scripts/DisplayLight.cs
+++ Logic Gates Educational Game/Assets/GridSystem/Scripts/DisplayLight.cs
@@ -7,8 +7,8 @@ public class DisplayLight : MonoBehaviour
     private bool value, valueOut;
     public CircuitInputTrigger input;
     public SpriteRenderer sprite;
-    private static Color onColor = new Color(0, 1, 0, 1);
-    private static Color offColor = new Color(1, 0, 0, 1);
+    private static Color onColor = new Color(1, 1, 0, 1);
+    private static Color offColor = new Color(46f / 255f, 46f / 255f, 46f / 255f, 1);
 
     public void setValue(bool value)
     {
--- Logic Gates Educational Game/Assets/GridSystem/Scripts/Switch.cs
+++ Logic Gates Educational Game/Assets/GridSystem/Scripts/Switch.cs
@@ -7,8 +7,9 @@ public class Switch : MonoBehaviour
     public bool value = false;
     public Wire output;
     public SpriteRenderer sprite;
-    private static Color onColor = new Color(0, 1, 0, 1);
-    private static Color offColor = new Color(1, 0, 0, 1);
+
+    private static Color onColor = new Color(1, 1, 0, 1);
+    private static Color offColor = new Color(46f / 255f, 46f / 255f, 46f / 255f, 1);
     void OnMouseDown()
     {
         Debug.Log("Clicked");
