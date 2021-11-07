7e3d7c1bd3e8bc1660192850a2c19fea66bfa7c9
Joseph Weir <2365352w@student.gla.ac.uk>
2021-11-07 18:20:33 +0000

    basic EditMode test setup

---
+++ Logic Gates Educational Game/Assets/Tests/EditMode/FirstTest.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class FirstTest
+{
+    [Test]
+    public void MyFirstTest()
+    {
+        Assert.AreEqual(1, 1);
+    }
+}
