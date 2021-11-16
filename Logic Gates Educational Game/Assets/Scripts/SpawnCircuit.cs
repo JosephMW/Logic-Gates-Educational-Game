f5dfa6f1950e886e473f5c542d5172dc2fac746f
Joseph Weir <2365352w@student.gla.ac.uk>
2021-11-16 20:22:47 +0000

    Now can drag new and2circuit onto scene

--- Logic Gates Educational Game/Assets/Scripts/SpawnCircuit.cs
+++ Logic Gates Educational Game/Assets/Scripts/SpawnCircuit.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class SpawnCircuit : MonoBehaviour
 {
     public GameObject objectToSpawn;
+    public CustomGrid customGrid;
+
     private void OnMouseDown()
     {
-        Debug.Log("You clicked the big red button");
-
         Vector3 mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
-        Instantiate(objectToSpawn, mousePosition + new Vector3(0, 1, 0), Quaternion.identity);
+        GameObject newObject = Instantiate(objectToSpawn, mousePosition, Quaternion.identity);
+        DragAndDrop dragAndDrop = newObject.GetComponent<DragAndDrop>();
+        dragAndDrop.customGrid = this.customGrid;
+        dragAndDrop.dragging = true;
     }
 }
