14e5e4ed2f35cffd606ca5b3da56962bd1c9bdd1
Joseph Weir <2365352w@student.gla.ac.uk>
2021-11-16 21:12:24 +0000

    refactored Toolbar to only use UI elements

--- Logic Gates Educational Game/Assets/Scripts/SpawnCircuit.cs
+++ Logic Gates Educational Game/Assets/Scripts/SpawnCircuit.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpawnCircuit : MonoBehaviour
 {
     public GameObject objectToSpawn;
     public CustomGrid customGrid;
 
-    private void OnMouseDown()
+    void Start()
+    {
+        this.gameObject.GetComponent<Button>().onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
     {
         Vector3 mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
