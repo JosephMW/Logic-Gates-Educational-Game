aaeb9d6214aad9a307ff90576377bd42222b5768
Joseph Weir <2365352w@student.gla.ac.uk>
2022-01-23 20:36:15 +0000

    mapped out LoadController - not sure of saving types. instantiation happens before sceneLoad

---
+++ Logic Gates Educational Game/Assets/Scripts/LoadController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static SaveController;
+
+public class LoadController : MonoBehaviour
+{
+    public GameObject passThroughCirc; //This is temporary!
+    public CustomGrid customGrid; //This is temporary!
+    public void LoadScene()
+    {
+        string saveName = "FirstSave";
+
+        Debug.Log("Load Scene called");
+
+        SceneManager.LoadSceneAsync("NewGame");
+
+
+        string fileContents = System.IO.File.ReadAllText(Application.dataPath + "/SaveFiles/" + saveName + ".json");
+        // Debug.Log(fileContents);
+        SaveFormat saveFormat = JsonUtility.FromJson<SaveFormat>(fileContents);
+        // Debug.Log(saveFormat.circuitJsons.Count);
+        for (int i = 0; i < saveFormat.circuitJsons.Count; i++)
+        {
+            string currentCircuitJson = saveFormat.circuitJsons[i];
+
+            CircuitSaveFormat circuitSaveFormat = JsonUtility.FromJson<CircuitSaveFormat>(currentCircuitJson);
+
+            for (int j = 0; j < circuitSaveFormat.inputPortJsons.Count; j++)
+            {
+                string currentInputPortJson = circuitSaveFormat.inputPortJsons[j];
+
+                InputPortSaveFormat inputPortSaveFormat = JsonUtility.FromJson<InputPortSaveFormat>(currentInputPortJson);
+            }
+
+            for (int k = 0; k < circuitSaveFormat.wireJsons.Count; k++)
+            {
+                string currentWireJson = circuitSaveFormat.wireJsons[k];
+
+                WireSaveFormat wireSaveFormat = JsonUtility.FromJson<WireSaveFormat>(currentWireJson);
+            }
+
+            // Debug.Log("circuitSaveFormat.circuitType");
+            // Debug.Log(circuitSaveFormat.circuitType);
+            // Debug.Log("circuitSaveFormat.circuitID");
+            // Debug.Log(circuitSaveFormat.circuitID);
+
+            GameObject newObject = Instantiate(passThroughCirc, circuitSaveFormat.location, Quaternion.identity);
+            // DragAndDrop dragAndDrop = newObject.GetComponent<DragAndDrop>();
+            // dragAndDrop.customGrid = this.customGrid;
+            Debug.Log(newObject);
+        }
+    }
+}
