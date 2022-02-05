d7f7a038d01ce4551260c063342754dd1396be50
Joseph Weir <2365352w@student.gla.ac.uk>
2022-02-05 13:35:27 +0000

    resized grid and cogwheel

--- Logic Gates Educational Game/Assets/Scripts/CustomGrid.cs
+++ Logic Gates Educational Game/Assets/Scripts/CustomGrid.cs
@@ -17,6 +17,9 @@ public class CustomGrid : MonoBehaviour
         {
             for (int y = 0; y < height; y++)
             {
+                if (x == 0 && y == height - 1)
+                    continue;
+
                 createBackgroundSquare(this.transform, convertGridArrayPositionToLocalPosition(x, y));
             }
         }
