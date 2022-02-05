ba7bd7b1c8eda566d695de72f6c2c0a183e6bb93
Joseph Weir <2365352w@student.gla.ac.uk>
2022-02-05 12:49:46 +0000

    Basic HideSpawnerButton styling

--- Logic Gates Educational Game/Assets/Scripts/SpawnCircuitButtonListController.cs
+++ Logic Gates Educational Game/Assets/Scripts/SpawnCircuitButtonListController.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using TMPro;
 
 public class SpawnCircuitButtonListController : MonoBehaviour
 {
     public GameObject buttonTemplate;
     private bool collapsed = false;
+    public TextMeshProUGUI textMeshPro;
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +31,7 @@ public class SpawnCircuitButtonListController : MonoBehaviour
     public void collapseList()
     {
         this.transform.localPosition = this.transform.localPosition + new Vector3(0, (collapsed ? 200 : -200), 0);
+        textMeshPro.text = collapsed ? "Hide Spawner" : "Show Spawner";
         this.collapsed = !this.collapsed;
     }
 }
