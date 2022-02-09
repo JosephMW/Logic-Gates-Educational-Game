622b94159dde172a569799659e6246c254990954
Joseph Weir <2365352w@student.gla.ac.uk>
2022-02-09 10:40:23 +0000

    New color scheme with alternating button colors

--- Logic Gates Educational Game/Assets/Scripts/SpawnCircuitButtonListController.cs
+++ Logic Gates Educational Game/Assets/Scripts/SpawnCircuitButtonListController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class SpawnCircuitButtonListController : MonoBehaviour
@@ -10,11 +11,14 @@ public class SpawnCircuitButtonListController : MonoBehaviour
     private bool collapsed = false;
     public TextMeshProUGUI textMeshPro;
 
+    public Color[] colorList;
+
     // Start is called before the first frame update
     void Start()
     {
         DirectoryInfo d = new DirectoryInfo(Application.dataPath + "/Resources/Sprites/Circuits/");
 
+        int colorCounter = 0;
         foreach (var file in d.GetFiles("*.prefab"))
         {
             string circuitName = file.Name.Substring(0, file.Name.Length - 7);
@@ -24,7 +28,13 @@ public class SpawnCircuitButtonListController : MonoBehaviour
             SpawnCircuitButton spawnCircuitButton = button.GetComponent<SpawnCircuitButton>();
             spawnCircuitButton.setObjectToSpawn((UnityEngine.GameObject)Resources.Load("Sprites/Circuits/" + circuitName));
             spawnCircuitButton.setText(circuitName);
+
+            button.GetComponent<Image>().color = colorList[colorCounter % colorList.Length];
+            Debug.Log(button.GetComponent<Image>().color);
+
             button.transform.SetParent(buttonTemplate.transform.parent, false);
+
+            colorCounter++;
         }
     }
 
