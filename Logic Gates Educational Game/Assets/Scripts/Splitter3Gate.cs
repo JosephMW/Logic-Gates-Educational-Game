a4ff7b18c6a721d24f671774fb1a3c4d5c84031f
Joseph Weir <2365352w@student.gla.ac.uk>
2022-02-15 18:49:35 +0000

    All four splitters added

--- Logic Gates Educational Game/Assets/Scripts/Splitter3Gate.cs
+++ Logic Gates Educational Game/Assets/Scripts/Splitter3Gate.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Splitter2Gate : CircuitParent
+public class Splitter3Gate : CircuitParent
 {
     public override bool[] calculateOutputs()
     {
-        // output is just the input twice
-        bool[] output = { inputValues[0], inputValues[0] };
+        // output is just the input 3 times
+        bool[] output = { inputValues[0], inputValues[0], inputValues[0] };
         return output;
     }
 }
