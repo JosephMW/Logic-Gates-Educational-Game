691d445e359e8bfb732d9e778432dc3ed547433f
Joseph Weir <2365352w@student.gla.ac.uk>
2022-02-16 19:34:24 +0000

    Now circuits working for Or2 only

--- Logic Gates Educational Game/Assets/Scripts/GlobalVariables.cs
+++ Logic Gates Educational Game/Assets/Scripts/GlobalVariables.cs
@@ -13,17 +13,22 @@ public class GlobalVariables : MonoBehaviour
     {
         typeDictionary = new Dictionary<string, CircuitParent>();
 
-        DirectoryInfo d = new DirectoryInfo(Application.dataPath + "/Resources/Sprites/Circuits/");
+        // DirectoryInfo d = new DirectoryInfo(Application.dataPath + "/Resources/Sprites/Circuits/");
 
-        foreach (var file in d.GetFiles("*.prefab"))
-        {
-            string circuitName = file.Name.Substring(0, file.Name.Length - 7);
+        // foreach (var file in d.GetFiles("*.prefab"))
+        // {
+        //     string circuitName = file.Name.Substring(0, file.Name.Length - 7);
 
-            GameObject circuitGameObject = (UnityEngine.GameObject)Resources.Load("Sprites/Circuits/" + circuitName);
-            CircuitParent circuitParent = circuitGameObject.GetComponent<CircuitParent>();
+        //     GameObject circuitGameObject = (UnityEngine.GameObject)Resources.Load("Sprites/Circuits/" + circuitName);
+        //     CircuitParent circuitParent = circuitGameObject.GetComponent<CircuitParent>();
 
-            typeDictionary.Add(circuitParent.GetType().ToString(), circuitParent);
-        }
+        //     typeDictionary.Add(circuitParent.GetType().ToString(), circuitParent);
+        // }
+
+        GameObject circuitGameObject = (UnityEngine.GameObject)Resources.Load("Sprites/Circuits/Or2");
+        CircuitParent circuitParent = circuitGameObject.GetComponent<CircuitParent>();
+
+        typeDictionary.Add(circuitParent.GetType().ToString(), circuitParent);
     }
 
     public static int getID()
